9cee459f7ac21e8a495f4fec851b06a5335c6fbd
Dylan Acup <github.zj664@simplelogin.co>
2024-05-20 20:15:22 -0500

    Solved code challenge

---
+++ CSCodeChallenges/Multiples3sAnd5s/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Multiples3sAnd5s
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Code Challenge 1");
+            // Find the sum of all multiples of 3 or 5 below 1000 and print it out to the console.
+
+            //int total = 0;
+
+            //for (int i = 1; i < 1000; i++)
+            //{
+            //    if (i % 3 == 0 || i % 5 == 0)
+            //        total += i;
+            //}
+
+            //Console.WriteLine(total);
+
+            //int total = 0;
+
+            //for (int i = 1; i < 1000; i++)
+            //{
+            //    if ((i - 3 * (i / 3)) == 0 || ((i - 5 * (i / 5)) == 0))
+            //        total += i;
+            //}
+
+            //Console.WriteLine(total);
+
+            int total = 0;
+
+            for (int i = 1; i < 1000; i++)
+            {
+                //try
+                //{
+                //    int test = int.Parse((i / 3m).ToString());
+                //    total += i;
+                //    continue;
+
+                //}
+                //catch (Exception e)
+                //{
+
+                //}
+
+
+                //try
+                //{
+                //    int test = int.Parse((i / 5m).ToString());
+                //    total += i;
+                //}
+                //catch (Exception e)
+                //{
+                //}
+
+                bool success3 = int.TryParse((i / 3m).ToString(), out int num3);
+                bool success5 = int.TryParse((i / 5m).ToString(), out int num5);
+
+                if (success3 || success5)
+                    total += i;
+
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
